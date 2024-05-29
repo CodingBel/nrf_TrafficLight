0bf172d09c4a7f71b21779e4a4d278d71c2981d7
CodingBel <84908127+CodingBel@users.noreply.github.com>
2024-05-29 12:24:00 +0200

    first Commit

--- src/main.c
+++ src/main.c
@@ -1,43 +1,46 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
+//#include<zephyr/zephyr.h>
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
+#include<zephyr/drivers/gpio.h>
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#define RED_SLEEP_TIME_MS 			2000
+#define RED_YELLOW_SLEEP_TIME_MS	500
+#define GREEN_SLEEP_TIME_MS			2000
+#define YELLOW_SLEEP_TIME_MS		500
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
+#define RED_NODE DT_NODELABEL(ledred)
+#define YELLOW_NODE DT_NODELABEL(ledyellow)
+#define GREEN_NODE DT_NODELABEL(ledgreen)
 
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec ledred_spec = GPIO_DT_SPEC_GET(RED_NODE, gpios);
+static const struct gpio_dt_spec ledyellow_spec = GPIO_DT_SPEC_GET(YELLOW_NODE, gpios);
+static const struct gpio_dt_spec ledgreen_spec = GPIO_DT_SPEC_GET(GREEN_NODE, gpios);
 
-int main(void)
-{
-	int ret;
+int main () {
+	gpio_pin_configure_dt(&ledred_spec, GPIO_OUTPUT);
+	gpio_pin_configure_dt(&ledyellow_spec, GPIO_OUTPUT);
+	gpio_pin_configure_dt(&ledgreen_spec, GPIO_OUTPUT);
 
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
+	while (1){
+		gpio_pin_set_dt(&ledred_spec, 1);
+		gpio_pin_set_dt(&ledyellow_spec, 0);
+		gpio_pin_set_dt(&ledgreen_spec, 0);
+		k_msleep(RED_SLEEP_TIME_MS);
+
+		gpio_pin_set_dt(&ledred_spec, 1);
+		gpio_pin_set_dt(&ledyellow_spec, 1);
+		gpio_pin_set_dt(&ledgreen_spec, 0);
+		k_msleep(RED_YELLOW_SLEEP_TIME_MS);
+
+		gpio_pin_set_dt(&ledred_spec, 0);
+		gpio_pin_set_dt(&ledyellow_spec, 0);
+		gpio_pin_set_dt(&ledgreen_spec, 1);
+		k_msleep(GREEN_SLEEP_TIME_MS);
+
+		gpio_pin_set_dt(&ledred_spec, 0);
+		gpio_pin_set_dt(&ledyellow_spec, 1);
+		gpio_pin_set_dt(&ledgreen_spec, 0);
+		k_msleep(YELLOW_SLEEP_TIME_MS);
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return 0;
-	}
-
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
-		}
-		k_msleep(SLEEP_TIME_MS);
-	}
-	return 0;
+	return 0; 
 }
